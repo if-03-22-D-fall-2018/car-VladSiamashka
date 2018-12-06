a77924ee073e3e29419736d013742e0be6991cce
Siamashka Vlad <wladsemashko@gmail.com>
2018-12-06 13:35:01 +0100

    6 tests left

--- car.cpp
+++ car.cpp
@@ -6,26 +6,35 @@ struct CarImplementation{
   int speed;
   double fill_level;
   double acceleration_rate;
+  double max_acceleration_rate;
+  double min_acceleration_rate;
   bool available;
 };
 
-#define MAXCARS 3
+#define MAXCARS 6
 
-struct CarImplementation aixam = {AIXAM, RED, 0, 16.0, 0, true};
-struct CarImplementation multipla = {FIAT_MULTIPLA, GREEN, 0, 65.0, 0, true};
-struct CarImplementation jeep = {JEEP, RED, 0, 80.0, 0, true};
+struct CarImplementation aixam = {AIXAM, RED, 0, 16.0, 0, 1.0,-8.0, true};
+struct CarImplementation multipla = {FIAT_MULTIPLA, GREEN, 0, 65.0, 0,2.26 ,-8.0, true};
+struct CarImplementation multipla2 = {FIAT_MULTIPLA, BLUE, 0, 65.0, 0,2.26 ,-8.0, true};
+struct CarImplementation multipla3 = {FIAT_MULTIPLA, ORANGE, 0, 65.0, 0,2.26 ,-8.0, true};
+struct CarImplementation jeep = {JEEP, SILVER, 0, 80.0, 0, 3.14,-8.0, true};
+struct CarImplementation jeep2 = {JEEP, BLACK, 0, 80.0, 0, 3.14,-8.0, true};
 
-Car car_park[MAXCARS] = {&aixam, &multipla, &jeep };
+Car car_park[MAXCARS] = {&aixam, &multipla, &multipla2, &multipla3, &jeep, &jeep2 };
 
 void init()
 {
-
+  for (int i = 0; i < MAXCARS; i++) {
+    car_park[i]->available=true;
+    car_park[i]->acceleration_rate=0;
+  }
 }
 
 Car get_car(Type type)
 {
   for (int i = 0; i < MAXCARS ; i++) {
     if (car_park[i]->type==type && car_park[i]->available==true) {
+      car_park[i]->available=false;
       return car_park[i];
     }
   }
@@ -45,15 +54,21 @@ double get_fill_level(Car car)
 }
 double get_acceleration_rate(Car car)
 {
-  return 0.0;
+  return car->acceleration_rate;
 }
-void set_acceleration_rate(Car, double rate)
+void set_acceleration_rate(Car car, double rate)
 {
-
+  if (rate <= car->max_acceleration_rate) {
+    car->acceleration_rate=rate;
+  }
+  else{
+    car->acceleration_rate=car->max_acceleration_rate;
+  }
+  //car->acceleration_rate=rate;
 }
 int get_speed(Car car)
 {
-  return 0;
+  return car->speed;
 }
 void accelerate(Car car)
 {
