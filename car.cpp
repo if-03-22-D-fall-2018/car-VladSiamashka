db69c426636ebd7b596e351b76a244bf1e76597b
SiamashkaVlad <wladsemashko@gmail.com>
2018-12-18 18:49:12 +0100

    evrythink works

--- car.cpp
+++ car.cpp
@@ -3,22 +3,23 @@
 struct CarImplementation{
   enum Type type;
   enum Color color;
-  int speed;
+  double speed;
   double fill_level;
   double acceleration_rate;
   double max_acceleration_rate;
   double min_acceleration_rate;
   bool available;
+  int max_speed;
 };
 
 #define MAXCARS 6
 
-struct CarImplementation aixam = {AIXAM, RED, 0, 16.0, 0, 1.0,-8.0, true};
-struct CarImplementation multipla = {FIAT_MULTIPLA, GREEN, 0, 65.0, 0,2.26 ,-8.0, true};
-struct CarImplementation multipla2 = {FIAT_MULTIPLA, BLUE, 0, 65.0, 0,2.26 ,-8.0, true};
-struct CarImplementation multipla3 = {FIAT_MULTIPLA, ORANGE, 0, 65.0, 0,2.26 ,-8.0, true};
-struct CarImplementation jeep = {JEEP, SILVER, 0, 80.0, 0, 3.14,-8.0, true};
-struct CarImplementation jeep2 = {JEEP, BLACK, 0, 80.0, 0, 3.14,-8.0, true};
+struct CarImplementation aixam = {AIXAM, RED, 0.0, 16.0, 0.0, 1.0,-8.0, true, 45};
+struct CarImplementation multipla = {FIAT_MULTIPLA, GREEN, 0.0, 65.0, 0.0,2.26 ,-8.0, true, 170};
+struct CarImplementation multipla2 = {FIAT_MULTIPLA, BLUE, 0.0, 65.0, 0.0,2.26 ,-8.0, true, 170};
+struct CarImplementation multipla3 = {FIAT_MULTIPLA, ORANGE, 0.0, 65.0, 0.0,2.26 ,-8.0, true, 170};
+struct CarImplementation jeep = {JEEP, SILVER, 0.0, 80.0, 0.0, 3.14,-8.0, true, 196};
+struct CarImplementation jeep2 = {JEEP, BLACK, 0.0, 80.0, 0.0, 3.14,-8.0, true, 196};
 
 Car car_park[MAXCARS] = {&aixam, &multipla, &multipla2, &multipla3, &jeep, &jeep2 };
 
@@ -27,6 +28,7 @@ void init()
   for (int i = 0; i < MAXCARS; i++) {
     car_park[i]->available=true;
     car_park[i]->acceleration_rate=0;
+    car_park[i]->speed=0;
   }
 }
 
@@ -58,19 +60,31 @@ double get_acceleration_rate(Car car)
 }
 void set_acceleration_rate(Car car, double rate)
 {
-  if (rate <= car->max_acceleration_rate) {
+  if (rate <= car->max_acceleration_rate&&rate>=car->min_acceleration_rate) {
     car->acceleration_rate=rate;
+  }
+  else if(rate<0){
+    car->acceleration_rate=car->min_acceleration_rate;
   }
   else{
     car->acceleration_rate=car->max_acceleration_rate;
   }
-  //car->acceleration_rate=rate;
 }
 int get_speed(Car car)
 {
-  return car->speed;
+  if (car->speed - (int)car->speed>=0.5) {
+    return (int)car->speed+1;
+  }
+  return (int)car->speed;
 }
 void accelerate(Car car)
 {
-
+  double speed=car->acceleration_rate*3.6;
+  if (car->speed+speed < car->max_speed) {
+    car->speed+=speed;
+  }
+  else if (car->speed < car->max_speed){
+    speed=car->max_speed-car->speed;
+    car->speed+=speed;
+  }
 }
